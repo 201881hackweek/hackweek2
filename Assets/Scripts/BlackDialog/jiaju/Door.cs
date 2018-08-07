7106c2e121a9eb5596ebb2768ed382410de8cdac
copen223 <40932824+copen223@users.noreply.github.com>
2018-08-07 18:32:55 +0800

    Add files via upload

---
+++ Assets/Scripts/BlackDialog/jiaju/Door.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool isOpended;
+    public bool hasOpended;// 事件门需要先用钥匙开启
+    public GameObject door;
+    public GameObject parent;
+
+    void Start()
+    {
+        hasOpended = false;      // 事件门改成false
+        isOpended = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("1");
+        if (!hasOpended)
+            return;
+        if (collision.tag != "Player")
+            return;
+        Debug.Log("2");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!isOpended)
+            {
+                collision.SendMessage("OpenDoor");
+                parent.transform.Rotate(90 * Vector3.up);
+                parent.transform.position += new Vector3(0.5f, 0);
+                door.SetActive(false);
+                isOpended = true;
+            }
+            else
+            {
+                collision.SendMessage("CloseDoor");
+                parent.transform.Rotate(-90 * Vector3.up);
+                parent.transform.position += new Vector3(-0.5f, 0);
+                door.SetActive(true);
+                isOpended = false;
+            }
+        }
+    }
+
+
+   
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
