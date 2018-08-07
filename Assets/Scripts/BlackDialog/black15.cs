613f31923ab9f458eb5b233b3bc3ac6ce1b55b5a
copen223 <40932824+copen223@users.noreply.github.com>
2018-08-07 18:38:33 +0800

    Add files via upload

---
+++ Assets/Scripts/BlackDialog/black15.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class black15: MonoBehaviour
+{
+    private SceneEventManager sceneeventmanager;
+    public Color coloroffset;
+    private Color origincolor;
+    private bool flag = false;//判断在黑屏结束后是否先按下space 再检测keyup 否则可能直接又开始黑屏
+    // Use this for initialization
+    void Start()
+    {
+        GameObject propmanager = GameObject.Find("SceneEventManager");
+        sceneeventmanager = propmanager.GetComponent<SceneEventManager>();
+        coloroffset = new Color(0.1f, 0.1f, 0.1f, 0);
+        origincolor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!sceneeventmanager.Blackscreen.activeSelf&&Time.time>sceneeventmanager.blackfinishtime+0.1f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<SpriteRenderer>().color = origincolor - coloroffset;
+                flag = true;
+            }
+
+
+            if (flag&&Input.GetKeyUp(KeyCode.Space))
+            {
+                GetComponent<SpriteRenderer>().color = origincolor;
+                sceneeventmanager.Blackscreen.SetActive(true);
+                sceneeventmanager.space = true;
+				sceneeventmanager.blacknumber = 15;
+                flag = false;
+            }
+        }
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().color = origincolor - coloroffset;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = origincolor;
+    }
+
+    private void OnMouseUp()
+    {
+        sceneeventmanager.Blackscreen.SetActive(true);
+		sceneeventmanager.blacknumber = 15;
+    }
+
+}
---
+++ Assets/Scripts/BlackDialog/dialog1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialog1 : MonoBehaviour
+{
+    private SceneEventManager sceneeventmanager;
+    public Color coloroffset;
+    private Color origincolor;
+    private bool flag = false;//判断在黑屏结束后是否先按下space 再检测keyup 否则可能直接又开始黑屏
+    // Use this for initialization
+    void Start()
+    {
+        GameObject propmanager = GameObject.Find("SceneEventManager");
+        sceneeventmanager = propmanager.GetComponent<SceneEventManager>();
+        coloroffset = new Color(0.1f, 0.1f, 0.1f, 0);
+        origincolor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!sceneeventmanager.Dialogbox.activeSelf&&Time.time>sceneeventmanager.generalfinishtime+0.1f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<SpriteRenderer>().color = origincolor - coloroffset;
+                flag = true;
+            }
+
+
+            if (flag&&Input.GetKeyUp(KeyCode.Space))
+            {
+                GetComponent<SpriteRenderer>().color = origincolor;
+                sceneeventmanager.Dialogbox.SetActive(true);
+                sceneeventmanager.space = true;
+                sceneeventmanager.generalnumber = 1;
+                flag = false;
+            }
+        }
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().color = origincolor - coloroffset;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = origincolor;
+    }
+
+    private void OnMouseUp()
+    {
+        sceneeventmanager.Dialogbox.SetActive(true);
+        sceneeventmanager.generalnumber = 1;
+    }
+
+}
